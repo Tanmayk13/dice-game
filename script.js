7f90db76271d7b10ffe1c10c7babce26aa150232
Tanmay Khilari <93090447+Tanmayk13@users.noreply.github.com>
2024-01-20 15:25:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let randomNumber1 = Math.floor(Math.random() * 6) + 1
+
+let p1 = document.querySelector(".img1")
+p1.setAttribute("src",`./images/dice${randomNumber1}.png`)
+
+let randomNumber2 = Math.floor(Math.random() * 6) + 1
+
+let p2 = document.querySelector(".img2")
+p2.setAttribute("src",`./images/dice${randomNumber2}.png`)
+
+let title = document.querySelector("h1")
+
+if(randomNumber1 > randomNumber2){
+    title.innerHTML = "Player 1 Wins!"
+} else if(randomNumber1 < randomNumber2){
+    title.textContent = "Player 2 Wins!"
+}else{
+    title.textContent = "It's A Draw!"
+}
+
+let button = document.querySelector(".btn")
+
+button.addEventListener('click', () =>{
+    location.reload();
+})
